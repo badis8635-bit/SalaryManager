442072a2e934b8b524084d7aaedf75b2bfa8c61a
Badis Saada <b.saada@stag.technofuturtic.education>
2026-05-20 11:29:57 +0200

    PayslipRoute

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from db.database import Base, engine
 from Routes.WorkerRoute import router as worker_router
 from Routes.AdminRoute import router as admin_router
 from Routes.ContractRoute import router as contract_router
+from Routes.PayslipRoute import router as payslip_router
 
-# Importer tous les modèles pour que SQLAlchemy les connaisse
 import Models.Worker
 import Models.Contract
 import Models.Payslip
@@ -16,3 +16,4 @@ app = FastAPI(title="Salary Manager API")
 app.include_router(worker_router)
 app.include_router(admin_router)
 app.include_router(contract_router)
+app.include_router(payslip_router)
